28e95d00af4fd3d0ee53389f7108cf1790f523f4
Ihor Liutak <ihorlt@gmail.com>
2022-10-28 13:21:19 +0300

    added images

---
+++ js/catatalog.js
@@ -0,0 +1,18 @@
+let cards = [
+    {
+        id: 1,
+        img: "img/fruits/Strawberries.png",
+        alt: "Strawberries",
+        title: "Strawberries",
+        cardText: ""
+    },
+    {
+        id: 2,
+        img: "img/fruits/Blueberries.png",
+        alt: "Blueberries",
+        title: "Blueberries",
+        cardText: ""
+    }
+];
+
+console.log(cards);
